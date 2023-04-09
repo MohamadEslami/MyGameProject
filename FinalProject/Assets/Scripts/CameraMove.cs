c7bc5e59ca988ded67ceced4a691e813ef9bc72c
Mohammad.E Eslami <mohamad.slami@gmail.com>
2023-04-09 14:09:14 -0700

    Fix_Bugs
    Fix some bugs

--- FinalProject/Assets/Scripts/CameraMove.cs
+++ FinalProject/Assets/Scripts/CameraMove.cs
@@ -8,12 +8,12 @@ public class CameraMove : MonoBehaviour
     public float MoveSpeed;
 
     // Update is called once per frame
-    private void FixedUpdate()
+    void Update()
     {
         //Camera Move
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(Tank.position.x, Tank.position.y, transform.position.z), Time.deltaTime * MoveSpeed);
         //Camera Limit
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4, 4), Mathf.Clamp(transform.position.y, -3, 3), transform.position.z);
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4, 4), Mathf.Clamp(transform.position.y, -3, 3), transform.position.z);
 
     }
 }
